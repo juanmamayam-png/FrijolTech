f4feb8c3f1179ffc7c20e1900f133ba677c4c037
MiguelAM17 <juanmamayam@gmail.com>
2026-05-18 21:23:41 -0500

    Actualizacion de archivos

--- frijoltech-backend/src/domain/interfaces/ICampaÃ±aRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Campaña } from '../entities/Campaña';
-
-export interface ICampañaRepository {
-  crear(campaña: Omit<Campaña, 'id' | 'createdAt'>): Promise<Campaña>;
-  buscarPorId(id: number): Promise<Campaña | null>;
-  listarPorLote(loteId: number): Promise<Campaña[]>;
-}
